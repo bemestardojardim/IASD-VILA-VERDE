4fb0cc7571ec0e2efb54146f805da33024a1c9ab
Bem Estar Do Jardim <56318003+bemestardojardim@users.noreply.github.com>
2023-03-26 16:06:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function buttonClicked() {
     document.getElementById("mainDiv").textContent =
         "Button Clicked times: " + numButtonClicks;
 }
+function redirigir(url) {
+    window.location.href = url;
+}
+<button id="myButton">Click me</button>
+
+<script>
+document.getElementById("myButton").addEventListener("click", function() {
+  alert("Hello World!");
+});
+</script>
+
+
