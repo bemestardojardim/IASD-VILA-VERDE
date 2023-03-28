533303213877d79b3fc2327ec903bc7c22814cd0
Bem Estar Do Jardim <56318003+bemestardojardim@users.noreply.github.com>
2023-03-28 13:59:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,33 +18,7 @@ function toggleLista() {
 
 
 
-function generatePDF() {
-    var doc = new jsPDF();
-    var source = window.document.getElementsByTagName("body")[0];
-    doc.fromHTML(
-        source,
-        15,
-        15,
-        {
-            'width': 180
-        });
 
-    doc.save('archivo.pdf');
-}
 
-function downloadPrograma() {
-  var link = document.createElement("a");
-  link.download = "archivo.pdf";
-  link.href = "/archivo.pdf";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
 
-var checkbox = document.getElementsByName('item')[indice];
-if (checkbox.checked) {
-    // código a ejecutar si el checkbox está seleccionado
-} else {
-    // código a ejecutar si el checkbox no está seleccionado
-}
 
